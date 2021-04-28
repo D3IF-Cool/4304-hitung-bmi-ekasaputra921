2ca431101dd0b7218eeedc524cb70deb829156fd
Rahmad_Fitrianta_Eka_Saputra <ekasaputra921@gmail.com>
2021-04-28 11:15:11 +0700

    Menampilkan data dalam bentuk list

--- app/src/main/java/org/d3if4073/hitungbmi/ui/histori/HistoriFragment.kt
+++ app/src/main/java/org/d3if4073/hitungbmi/ui/histori/HistoriFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 import org.d3if4073.hitungbmi.databinding.FragmentHistoriBinding
 import org.d3if4073.hitungbmi.db.BmiDb
 
@@ -18,16 +20,28 @@ class HistoriFragment : Fragment() {
         ViewModelProvider(this, factory).get(HistoriViewModel::class.java)
     }
     private lateinit var binding: FragmentHistoriBinding
+    private lateinit var myAdapter: HistoriAdapter
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = FragmentHistoriBinding.inflate(layoutInflater,
             container, false)
+        myAdapter = HistoriAdapter()
+        with(binding.recyclerView) {
+            addItemDecoration(
+                DividerItemDecoration(context,
+                RecyclerView.VERTICAL)
+            )
+            adapter = myAdapter
+            setHasFixedSize(true)
+        }
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.data.observe(viewLifecycleOwner, {
-            Log.d("HistoriFragment", "Jumlah data: ${it.size}")
+            binding.emptyView.visibility = if (it.isEmpty())
+                View.VISIBLE else View.GONE
+            myAdapter.updateData(it)
         })
     }
 }
